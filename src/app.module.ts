13f6ee2dcd1c848d23b1b39cddc44ac5ff998d55
ManuelFBS <manuelf.borrego@gmail.com>
2025-07-05 19:35:07 -0400

    Actualización de "app.module.ts"...

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
-import { EmployeeService } from './core/use-cases/employees/employee.service';
+// import { EmployeeService } from './core/use-cases/employees/employee.service';
+// import { PrismaEmployeeRepository } from './infrastructure/repositories/employees/prisma-employee.repository';
 import { UserController } from './infrastructure/http/controllers/users/user.controller';
 import { UserService } from './core/use-cases/users/user.service';
 import { PrismaService } from './infrastructure/database/prisma/prisma.service';
