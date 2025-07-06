8e062b07c7a1bc5dee02e3195fd62a0e4bb3b96c
ManuelFBS <manuelf.borrego@gmail.com>
2025-07-06 03:29:13 -0400

    Modificaciones en 'jwt-payload.interface.ts' y 'jwt.strategy.ts'...

--- src/auth/interfaces/jwt-payload.interface.ts
+++ src/auth/interfaces/jwt-payload.interface.ts
@@ -1,5 +1,5 @@
 export interface JWTPayload {
-        sub: number; // User ID
+        sub: number; //> User ID...
         username: string;
         role: string;
         iat?: number;
--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -2,3 +2,21 @@ import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { AuthService } from '../auth.service';
+import { JWTPayload } from '../interfaces/jwt-payload.interface';
+
+@Injectable()
+export class JWTStrategy extends PassportStrategy(Strategy) {
+        constructor(private readonly authService: AuthService) {
+                super({
+                        jwtFromRequest:
+                                ExtractJwt.fromAuthHeaderAsBearerToken(),
+                        ignoreExpiration: false,
+                        secretOrKey:
+                                process.env.JWT_SECRET || 'fallback-secret-key',
+                });
+        }
+
+        async validate(payload: JWTPayload) {
+                return this.authService.validateUserByPayload(payload);
+        }
+}
