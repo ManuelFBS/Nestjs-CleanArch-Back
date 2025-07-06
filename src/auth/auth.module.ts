5ff6648584c0d0496aec866af52d19081378de46
ManuelFBS <manuelf.borrego@gmail.com>
2025-07-06 03:52:37 -0400

    Creación de 'auth.controller.ts'; Modific. de 'auth.module.ts'...

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
-//import{}
+import { UserModule } from '../modules/users/user.module';
+import { AuthService } from './auth.service';
+import { JWTStrategy } from './strategies/jwt.strategy';
+// import { AuthController } from './auth.controller';
---
+++ src/auth/controllers/auth.controller.ts
@@ -0,0 +1,20 @@
+import {
+        Controller,
+        Post,
+        Body,
+        UseGuards,
+        Req,
+        UnauthorizedException,
+} from '@nestjs/common';
+import { AuthService } from '../auth.service';
+// import { LocalAuthGuard } from './guards/local-auth.guard';
+// import { LoginDto } from '../dto/login.dto';
+// import { JwtAuthGuard } from './guards/jwt-auth.guard';
+// import { Permissions } from '../../core/permissions/permissions.decorator';
+// import { Permission } from '../../core/permissions/permission';
+
+@Controller('api/auth')
+export class AuthController {
+        constructor(private readonly authService: AuthService) {}
+        //
+}
