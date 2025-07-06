2f5ba3df236ced6c5dd6a66532efa9c3fef4e23f
ManuelFBS <manuelf.borrego@gmail.com>
2025-07-06 16:21:38 -0400

    Actaulización de 'auth.module.ts'...

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,4 +4,24 @@ import { PassportModule } from '@nestjs/passport';
 import { UserModule } from '../modules/users/user.module';
 import { AuthService } from './auth.service';
 import { JWTStrategy } from './strategies/jwt.strategy';
-// import { AuthController } from './auth.controller';
+import { AuthController } from '../auth/controllers/auth.controller';
+import { TokenBlacklistService } from '../auth/token/token-blacklist.service';
+import { RedisModule } from '../shared/redis/redis.module';
+
+@Module({
+        imports: [
+                PassportModule.register({ defaultStrategy: 'jwt' }),
+                JwtModule.register({
+                        secret: process.env.JWT_SECRET,
+                        signOptions: {
+                                expiresIn: process.env.JWT_EXPIRES_IN || '8h',
+                        },
+                }),
+                UserModule,
+                RedisModule,
+        ],
+        providers: [AuthService, JWTStrategy, TokenBlacklistService],
+        controllers: [AuthController],
+        exports: [JWTStrategy, PassportModule],
+})
+export class AuthModule {}
