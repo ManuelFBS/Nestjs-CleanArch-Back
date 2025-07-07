50ddcd1df5e74c911330f7c65ad49ff4c735e9d5
ManuelFBS <manuelf.borrego@gmail.com>
2025-07-07 12:05:05 -0400

    Corrección en employee.module.ts

--- src/modules/employees/employee.module.ts
+++ src/modules/employees/employee.module.ts
@@ -14,6 +14,6 @@ import { PrismaEmployeeRepository } from '../../infrastructure/repositories/empl
                         useClass: PrismaEmployeeRepository,
                 },
         ],
-        exports: [EmployeeService],
+        exports: [EmployeeService, 'EmployeeRepository'],
 })
 export class EmployeeModule {}
