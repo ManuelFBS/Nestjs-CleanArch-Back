e20c92f0abb59e28a908d5fcbe7788907634b699
Manuel F. Borrego S. <manuelf.borrego@gmail.com>
2025-07-08 02:30:30 -0400

    Actualz. de 'prisma.service.ts' y 'user.controller.ts'....

--- src/infrastructure/interface-adapters/controllers/users/user.controller.ts
+++ src/infrastructure/interface-adapters/controllers/users/user.controller.ts
@@ -8,6 +8,7 @@ import {
         Delete,
         ParseIntPipe,
         UnauthorizedException,
+        UseGuards,
 } from '@nestjs/common';
 import { UserService } from '../../../../application/use-cases/users/user.service';
 import {
@@ -15,6 +16,8 @@ import {
         UpdateUserDTO,
         UserResponseDTO,
 } from '../../../../application/dto/users/create-user.dto';
+import { JWTAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { Permissions } from '../../../../core/permissions/permissions.decorator';
 import { plainToInstance } from 'class-transformer';
 
 @Controller('api/users')
@@ -22,6 +25,8 @@ export class UserController {
         constructor(private readonly userService: UserService) {}
 
         @Post('newuser')
+        @UseGuards(JWTAuthGuard)
+        @Permissions('user:create')
         async create(
                 @Body() createUserDTO: CreateUserDTO,
         ): Promise<UserResponseDTO> {
@@ -31,6 +36,8 @@ export class UserController {
         }
 
         @Get()
+        @UseGuards(JWTAuthGuard)
+        @Permissions('user:read')
         async findAll(): Promise<UserResponseDTO[]> {
                 const users = await this.userService.findAllUsers();
 
@@ -38,6 +45,8 @@ export class UserController {
         }
 
         @Get('userbyid/:id')
+        @UseGuards(JWTAuthGuard)
+        @Permissions('user:read')
         async findOne(
                 @Param('id', ParseIntPipe) id: number,
         ): Promise<UserResponseDTO> {
@@ -47,6 +56,8 @@ export class UserController {
         }
 
         @Get('userbydni/:dni')
+        @UseGuards(JWTAuthGuard)
+        @Permissions('user:read')
         async findByDni(@Param('dni') dni: string): Promise<UserResponseDTO> {
                 const user = await this.userService.findUserByDNI(dni);
 
@@ -54,6 +65,8 @@ export class UserController {
         }
 
         @Put('userupd/:id')
+        @UseGuards(JWTAuthGuard)
+        @Permissions('user:update')
         async update(
                 @Param('id', ParseIntPipe) id: number,
                 @Body() updateUserDto: UpdateUserDTO,
@@ -67,22 +80,24 @@ export class UserController {
         }
 
         @Delete('userdel/:id')
+        @UseGuards(JWTAuthGuard)
+        @Permissions('user:update')
         async remove(@Param('id', ParseIntPipe) id: number): Promise<void> {
                 await this.userService.deleteUser(id);
         }
 
-        @Post('login')
-        async login(@Body() loginDto: { username: string; password: string }) {
-                const user = await this.userService.validateUser(
-                        loginDto.username,
-                        loginDto.password,
-                );
-                if (!user) {
-                        throw new UnauthorizedException(
-                                'Credenciales inválidas',
-                        );
-                }
-                //* Aquí deberías generar y retornar un token JWT...
-                return plainToInstance(UserResponseDTO, user);
-        }
+        // @Post('login')
+        // async login(@Body() loginDto: { username: string; password: string }) {
+        //         const user = await this.userService.validateUser(
+        //                 loginDto.username,
+        //                 loginDto.password,
+        //         );
+        //         if (!user) {
+        //                 throw new UnauthorizedException(
+        //                         'Credenciales inválidas',
+        //                 );
+        //         }
+        //         //* Aquí deberías generar y retornar un token JWT...
+        //         return plainToInstance(UserResponseDTO, user);
+        // }
 }
